d3e01957814fa8e4cf83f532b6f0fb2a37b71a71
ManasJhaMJ <jhamanas939@gmail.com>
2025-04-03 22:54:01 +0530

    Add audio

--- src/components/BattleBg.jsx
+++ src/components/BattleBg.jsx
@@ -1,3 +1,6 @@
+import React, { useState, useRef } from "react";
+import { Volume2, VolumeX } from "lucide-react";
+
 import bg1 from '../assets/bg/1.png'
 import bg2 from '../assets/bg/2.png'
 import bg3 from '../assets/bg/3.png'
@@ -8,6 +11,19 @@ import bg7 from '../assets/bg/7.png'
 import bg8 from '../assets/bg/8.png'
 
 function BattleBg() {
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(new Audio("/audio/pixel-song.mp3"));
+  audioRef.current.loop = true;
+  const toggleAudio = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+  
   return (
     <>
       <img src={bg1} className='bg-1' alt="bg-1" />
@@ -18,7 +34,12 @@ function BattleBg() {
       <img src={bg6} className='bg-6' alt="bg-6" />
       <img src={bg7} className='bg-7' alt="bg-7" />
       <img src={bg8} className='bg-8' alt="bg-8" />
-      
+
+      <div className="">
+      <button onClick={toggleAudio} className="audio-button">
+        {isPlaying ? <Volume2 size={24} /> : <VolumeX size={24} />}
+      </button>
+    </div>
     </>
   )
 }
